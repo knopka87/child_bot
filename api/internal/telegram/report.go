83da3fd2b6506ba1d355dffc36463227bcde7329
a.yanover <a.yanover@xsolla.com>
2025-10-20 21:16:00 +0300

    update Telegram report: uncomment and format admin chat ID list for better readability and maintainability

--- api/internal/telegram/report.go
+++ api/internal/telegram/report.go
@@ -19,7 +19,10 @@ import (
 )
 
 // Developer/admin chat to receive reports.
-var adminReportChatID = []int64{255509524 /*, 310452272*/}
+var adminReportChatID = []int64{
+	255509524,
+	310452272,
+}
 
 type imageFile struct {
 	Data []byte
