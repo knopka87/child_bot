57c144920565775fa68ed8c9ae7962e5f46e6553
a.yanover <a.yanover@xsolla.com>
2025-11-08 15:32:57 +0300

    update migration and deploy setup: fix parsed_tasks indices and constraints, add Docker Compose for deployment

--- api/migrations/013_update_task_sessions.up.sql
+++ api/migrations/013_update_task_sessions.up.sql
@@ -6,11 +6,7 @@ ALTER TABLE parsed_tasks
 DROP INDEX IF EXISTS parsed_tasks_image_hash_engine_idx;
 
 CREATE INDEX IF NOT EXISTS ix_parsed_tasks_chat_updated
-    ON childbot.parsed_tasks(chat_id, updated_at DESC);
+    ON parsed_tasks(chat_id, updated_at DESC);
 
-UPDATE childbot.parsed_tasks
-SET session_id = NULL
-WHERE session_id = '';
-
-ALTER TABLE childbot.parsed_tasks
+ALTER TABLE parsed_tasks
     ALTER COLUMN session_id SET NOT NULL;
